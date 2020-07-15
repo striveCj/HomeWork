94bbb8e115723b6bba0d81f905de6503e0328720
ChenJie <530216775@qq.com>
2020-07-16 01:25:06 +0800

    作业3,4实现

---
+++ HomeWork-1/DbHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Text;
+
+namespace HomeWork_1
+{
+    public class DbHelper<T> where T:BaseModel.BaseModel,new()
+    {
+        /// <summary>
+        /// 链接字符串，需要转到json配置中去
+        /// </summary>
+        private string _connString = @"Data Source=.;Initial Catalog=HomeWork;User ID=sa;Password=w1!";
+
+        /// <summary>
+        /// 查询所有数据
+        /// </summary>
+        /// <returns></returns>
+        public List<T> FindAll()
+        {
+            Type t = typeof(T);
+            using (SqlConnection conn=new SqlConnection(_connString))
+            {
+                conn.Open();
+                string sql = $"select * from [{t.Name}]";
+                SqlCommand comm = new SqlCommand(sql, conn);
+                SqlDataReader dr = comm.ExecuteReader();
+                return DataReaderToModel<T>(dr);
+            }
+        }
+
+        public T FindById(int id)
+        {
+            Type t = typeof(T);
+            using (SqlConnection conn = new SqlConnection(_connString))
+            {
+                conn.Open();
+                string sql = $"select * from [{t.Name}] where id=@id";
+                SqlCommand comm = new SqlCommand(sql, conn);
+                comm.Parameters.AddWithValue($"@id", id);
+                SqlDataReader dr = comm.ExecuteReader();
+                return DataReaderToModel<T>(dr)[0];
+            }
+        }
+
+        /// <summary>
+        /// DataReader转实体
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dr"></param>
+        /// <returns></returns>
+        public List<T> DataReaderToModel<T>(SqlDataReader dr)
+        {
+            var list = new List<T>();
+            T t = default;
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    t = (T)Activator.CreateInstance(typeof(T));
+                    for (int i = 0; i < dr.FieldCount; i++)
+                    {
+                        var p = t.GetType().GetProperty(dr.GetName(i), BindingFlags.Instance | BindingFlags.Public | BindingFlags.IgnoreCase);
+                        if (Convert.IsDBNull(dr.GetValue(i)))
+                        {
+                            p.SetValue(t, null, null);
+                        }
+                        else
+                        {
+                            p.SetValue(t, dr.GetValue(i), null);
+                        }
+                        list.Add(t);
+                    }
+                }
+            }
+            return list;
+        }
+    
+        
+        /// <summary>
+        /// 获取当前传入实体的所有属性，属性值
+        /// </summary>
+        /// <param name="t"></param>
+        public void GetTypeInfo(T t)
+        {
+            Type type = t.GetType();
+
+            var properties = type.GetProperties();
+
+            foreach (var item in properties)
+            {
+                Console.WriteLine($"当前属性:{item.Name}.对应属性值:{item.GetValue(t)}");
+            }
+        }
+    }
+
+}
--- HomeWork-1/Program.cs
+++ HomeWork-1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeWork_1.CodeGenerator;
+using System;
 using System.Collections.Generic;
 using System.Data;
 
@@ -8,13 +9,51 @@ namespace HomeWork_1
     {
         static void Main(string[] args)
         {
+
+            FindById();
+        }
+
+        /// <summary>
+        /// 实体代码生成器
+        /// </summary>
+        public static void CodeGeerator()
+        {
             Console.WriteLine("输入Y生成代码");
             string userInput = Console.ReadLine();
-            if (userInput.ToLower()=="y")
+            if (userInput.ToLower() == "y")
             {
                 new CodeGenerator.CodeGenerator().CodeRun();
             }
-
         }
+
+        public static void FindAll()
+        {
+            DbHelper<User> dbu = new DbHelper<User>();
+            DbHelper<Company> dbc = new DbHelper<Company>();
+            List<Company> list = dbc.FindAll();
+            List<User> userList = dbu.FindAll();
+
+            foreach (var item in list)
+            {
+                dbc.GetTypeInfo(item);
+            }
+            foreach (var item in userList)
+            {
+                dbu.GetTypeInfo(item);
+            }
+        }
+
+        public static void FindById()
+        {
+            DbHelper<User> dbu = new DbHelper<User>();
+            DbHelper<Company> dbc = new DbHelper<Company>();
+            Company company = dbc.FindById(1);
+            User user = dbu.FindById(1);
+
+            dbc.GetTypeInfo(company);
+            dbu.GetTypeInfo(user);
+        }
+
+       
     }
 }
