56170ba51defa82d51b4a460519c8621fe7c3de3
ChenJie <530216775@qq.com>
2020-07-28 23:21:10 +0800

    where实现

---
+++ HomeWork-1/ListExtension/WhereExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_1.ListExtension
+{
+    public static class WhereExtension
+    {
+        public static List<T> JakeWhere<T>(this List<T> enumerable,Func<T,bool> func) {
+
+            List<T> list = new List<T>();
+            enumerable.ForEach(i =>
+            {
+                if (func.Invoke(i))
+                {
+                    list.Add(i);
+                }
+            });
+            return list;
+        }
+    }
+}
