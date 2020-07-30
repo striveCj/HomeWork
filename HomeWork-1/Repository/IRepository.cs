645894d2b0273ac6420a049b7e0ffdc7591d0375
ChenJie <530216775@qq.com>
2020-07-30 23:36:01 +0800

    增加操作仓储

---
+++ HomeWork-1/Repository/IRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_1.Repository
+{
+    public interface IRepository<T> where T:BaseModel.BaseModel
+    {
+        /// <summary>
+        /// 获取所有数据
+        /// </summary>
+        /// <returns></returns>
+        public List<T> FindAll();
+
+        /// <summary>
+        /// 根据ID获取指定数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T FindById(int id);
+
+        /// <summary>
+        /// 新增方法
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public int Add(T t);
+
+        /// <summary>
+        /// 修改方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public int Update(T t);
+
+        /// <summary>
+        /// 删除方法
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int Delete(int id);
+    }
+}
